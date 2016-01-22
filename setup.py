1e10d3e057fde582a317e6b16270b148fd9e9cd0
Jinyang Zhou <ailurus1991@Jinyangs-iMac.local>
2016-01-22 11:49:41 +0800

    init

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='string_demon',
+      version='0.1',
+      description='String Feature Extraction',
+      url='http://github.com/guokr/string_demon',
+      author='Jinyang Zhou',
+      author_email='jinyang.zhou@guokr.com',
+      license='MIT',
+      packages=['string_demon'],
+      zip_safe=False)
