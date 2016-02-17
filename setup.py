badc4552a3f62a6d19db35108151422674c923ab
goodspeed <cacique1103@gmail.com>
2016-02-17 14:47:46 +0800

    add install requires

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
 from distutils.core import setup
 
+
 def readme():
     with open('README.rst') as f:
         return f.read()
 
-setup(name='string_demon',
-      version='0.2.42',
-      description='String Feature Extraction',
-      url='http://github.com/guokr/string-demon',
-      author='Jinyang Zhou',
-      author_email='jinyang.zhou@guokr.com',
-      license='MIT',
-      packages=['string_demon'],
-      zip_safe=False)
+
+setup(
+    name='string_demon',
+    version='0.2.42',
+    description='String Feature Extraction',
+    url='http://github.com/guokr/string-demon',
+    author='Jinyang Zhou',
+    author_email='jinyang.zhou@guokr.com',
+    license='MIT',
+    packages=['string_demon'],
+    zip_safe=False,
+    install_requires=['regex==2016.01.10']
+)
